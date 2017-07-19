ddc424831657cb7fc64774ffb49130684ddc563c
Umang <umangchavan20@gmail.com>
2017-07-18 22:25:30 -0500

    small changes

--- echoserver.py
+++ echoserver.py
@@ -10,21 +10,15 @@ PAT = ''
 
 @app.route('/', methods=['GET'])
 def handle_verification():
-  # print "Handling Verification."
   if request.args.get('hub.verify_token', '') == 'my_voice_is_my_password_verify_me':
-    # print "Verification successful!"
     return request.args.get('hub.challenge', '')
   else:
-    # print "Verification failed!"
     return 'Error, wrong validation token'
 
 @app.route('/', methods=['POST'])
 def handle_messages():
-  # print "Handling Messages"
   payload = request.get_data()
-  # print payload
   for sender, message in messaging_events(payload):
-    # print "Incoming from %s: %s" % (sender, message)
     send_message(PAT, sender, message)
   return "ok"
 
@@ -52,8 +46,6 @@ def send_message(token, recipient, text):
       "message": {"text": text.decode('unicode_escape')}
     }),
     headers={'Content-type': 'application/json'})
-  # if r.status_code != requests.codes.ok:
-    # print r.text
 
 if __name__ == '__main__':
   app.run()
