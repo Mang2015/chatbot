4e7fde2668555d562b68f54ca350d6ee3507f210
Umang <umangchavan20@gmail.com>
2017-07-20 23:09:58 -0500

    small change

--- echoserver.py
+++ echoserver.py
@@ -44,7 +44,7 @@ def send_message(token, recipient, text):
     params={"access_token": token},
     data=json.dumps({
       "recipient": {"id": recipient},
-      "message": {text": text.decode('unicode_escape')}
+      "message": {"text": text.decode('unicode_escape')}
     }),
     headers={'Content-type': 'application/json'})
 
