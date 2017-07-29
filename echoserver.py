8f1cd95aabfe09399006c007e323cc7f0972bc37
Umang <umangchavan20@gmail.com>
2017-07-29 18:36:20 -0500

    Cataloguing

--- echoserver.py
+++ echoserver.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 # by the Facebook App that will be created.
 
 PAT = 'EAAZAuhEvzHlYBAN6kiSrZBbGTvBFcVCNf8WLLcYtkvZCZAZAhnCdtAVzJ6ibbLapEJD1iYH0Jpd5QGOg9O4vy0NOZBEiCZAMWWLEbsZCyn6xGapxZB0e770lWEM8knGS48Lg9NOPwQIIlydhOBDWd7JrJ5ZCHfihxoO4TBcCoABdmfpgZDZD'
+information = {}
 
 @app.route('/', methods=['GET'])
 def handle_verification():
@@ -30,17 +31,41 @@ def messaging_events(payload):
   """
   data = json.loads(payload)
   messaging_events = data["entry"][0]["messaging"]
-  last_names = {"Rohit": "Thotakura", "Umang": "Chavan", "Priya": "Rajan", "Bisini": "Panicker"}
+
   for event in messaging_events:
     if "message" in event:
-        for name in last_names:
-            if name in event["message"]["text"]:
-              yield event["sender"]["id"], last_names[name].encode('unicode_escape')
-              success = True
-            else:
-                continue
-        if success != True:
-            yield event["sender"]["id"], "I can't echo this".encode('unicode_escape')
+        if "add" in event["message"]["text"]:
+            ret_message = add_user_info(event, event["sender"]["id"])
+        elif "list" in event["message"]["text"]:
+            ret_message = list_user_info(event, event["sender"]["id"])
+        else:
+            yield event["sender"]["id"], "This is not a recognized command".encode('unicode_escape')
+            break
+        yield event["sender"]["id"], ret_message.encode('unicode_escape')
+
+def add_user_info(event, sender):
+    question = "Full name of new entry"
+    send_message(PAT, sender, question)
+
+    new_user = event["message"]["text"]
+
+    question = "What information would you like to store?"
+    send_message(PAT, sender, question)
+
+    information[new_user] = event["message"]["text"]
+
+    return "success"
+
+def list_user_info(event, sender):
+    question = "Full name of user"
+    send_message(PAT, sender, question)
+    
+    for name in information:
+        if name in event["message"]["text"]:
+          return information[name]
+        else:
+            continue
+    return "No such user"
 
 def send_message(token, recipient, text):
   """Send the message text to recipient with id recipient.
@@ -53,6 +78,8 @@ def send_message(token, recipient, text):
       "message": {"text": text.decode('unicode_escape')}
     }),
     headers={'Content-type': 'application/json'})
+
+
 
 if __name__ == '__main__':
   app.run()
