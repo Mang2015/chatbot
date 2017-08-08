9db1d881a327a1a45a48d8328a9f7bc959a6d88c
Umang <umangchavan20@gmail.com>
2017-08-08 05:26:24 -0500

    defaulting

--- app/echoserver.py
+++ app/echoserver.py
@@ -131,7 +131,6 @@ def list_user_info(curr_state):
     user = User.query.filter_by(username = temp_message).first()
 
     if user:
-      send_message(PAT, temp_sender, temp_sender.encode("unicode_escape"))
       send_message(PAT, temp_sender, user.information.encode("unicode_escape"))
     else:
       send_message(PAT, temp_sender, "No such user".encode("unicode_escape"))
